7d5dfa5f4898ffcd1877cb45613bed6739247ca2
unknown <saiikiranboddea8142@gmail.com>
2026-06-09 13:31:43 +0530

    Fix CSS filename case

--- src/Student_Registration.jsx
+++ src/Student_Registration.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Student_registration.css";
+import "./Student_Registration.css";
 
 function Student_Registration() {
     const navigate = useNavigate();
